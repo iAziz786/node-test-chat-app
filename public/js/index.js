00241556c5c2b4e6f097277004b0d031986dcaee
Mohammmad Aziz <mdaziz067@gmail.com>
2017-07-01 22:37:10 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,12 +26,12 @@ socket.on('newLocationMessage', function (message) {
 
 $('#message-form').on('submit', function (e) {
   e.preventDefault();
-
+  var messageTextBox = $('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextBox.val()
   }, function () {
-
+    messageTextBox.val('');
   });
 });
 
@@ -40,12 +40,15 @@ locationButton.on('click', function() {
   if (!navigator.geolocation) {
     return alert('Sorry, geolocaiton is not supported by your browser.');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function (position) {
     socket.emit('getLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
+    locationButton.removeAttr('disabled').text('Sending location');
   }, function () {
+    locationButton.removeAttr('disabled').text('Sending location');
     alert('Unable to fetch the location');
   });
 });
